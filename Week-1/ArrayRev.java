fa90571d59b3942fff8b919e6cabe212571e4efd
Rishav Rai <88434875+Kenchin05@users.noreply.github.com>
2024-02-02 23:27:14 +0530

    Update ArrayRev.java

--- Week-1/ArrayRev.java
+++ Week-1/ArrayRev.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class rev{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ente the no of elements: ");
+        System.out.println("Enter the no of elements: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++){
